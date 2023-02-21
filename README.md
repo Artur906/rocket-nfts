78b0e7259c160b24f717e4e545b2cc81b3660c01
Artur Dantas <arturdantasrodrigues@gmail.com>
2023-02-21 13:25:12 -0300

    making the badge follow the cursor

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 Opa, o Rocket - NFTS é um desafio da rocketseat, eu peguei ele pra praticar um pouco o HTML e CSS, além disso usei Javascript para algumas manipulações de DOM.
 
-Também tive minha primeira experiência com animações tridimensionais!! 
+Também tive minha primeira experiência com animações em 3 dimensões!! 
 
-Da uma olhada em como ficou!! 👀 <br>
+Da uma olhada em [como ficou](https://artur906.github.io/rocket-nfts/)!! 👀 <br>
 ![](./public/demonstrating-project.gif)
 
 Proximos passos? 
 - [x] reponsividade 
 - [x] animações (animação 3d ;-;)
-- [ ] badge deve seguir o mouse
+- [x] badge deve seguir o mouse
--- script.js
+++ script.js
@@ -5,3 +5,40 @@ toggleMenuBtn.addEventListener('click', () => {
   toggleMenuBtn.classList.toggle('active')
   menu.classList.toggle('active')
 })
+
+// moviment badge with mouse 
+const badgeElement = document.querySelector('.badge')
+let badgePosition = badgeElement.getBoundingClientRect()
+let badgeCenterXPosition = badgePosition.x + 101;
+let badgeCenterYPosition = badgePosition.y + 101;
+
+const rotateBedge = (event) => {
+  const mouseX = event.clientX; 
+  const mouseY = event.clientY;
+
+  const offsetX = ((mouseX - badgeCenterXPosition) / badgeCenterXPosition) * 40; 
+  const offsetY = ((mouseY - badgeCenterYPosition) / badgeCenterYPosition) * 40; 
+  
+  const limitedX = (offsetX > 40 ? 40 : offsetX) < -40 ? -40 : offsetX;
+  const limitedY = (offsetY > 40 ? 40 : offsetY) < -40 ? -40 : offsetY;
+
+  badgeElement.style.setProperty("--x-rotation", limitedY * -1 + 'deg')
+  badgeElement.style.setProperty("--y-rotation", limitedX + 'deg')
+}
+
+addEventListener('resize', () => {
+  badgePosition = badgeElement.getBoundingClientRect()
+
+  // sorry for this magic numbers
+  badgeCenterXPosition = badgePosition.x + 101;
+  badgeCenterYPosition = badgePosition.y + 99;
+
+})
+
+addEventListener('mousemove', e => {
+  let isMobile = window.matchMedia("(any-pointer:coarse)").matches;
+
+  if(e.clientY < 600 && !isMobile) {
+    rotateBedge(e)
+  }
+})
